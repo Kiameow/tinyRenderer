9bc6786c54bd9407536d0dd22bb966e3732c4064
kiameow <zjzootl@qq.com>
2024-12-03 21:15:51 +0800

    feat: tgacolor [] operator

--- tgaimage.h
+++ tgaimage.h
@@ -73,6 +73,21 @@ struct TGAColor {
 
 		return c;
 	}
+
+	unsigned char& operator[](size_t i) {
+		switch(i) {
+			case 0:
+				return r;
+			case 1:
+				return g;
+			case 2:
+				return b;
+			case 3:
+				return a;
+			default:
+				throw "beyond the TGAColor boundary";
+		}
+	}
 };
 
 std::ostream& operator<<(std::ostream& out, TGAColor& color);
